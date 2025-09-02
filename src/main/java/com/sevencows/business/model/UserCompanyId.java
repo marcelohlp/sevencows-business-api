2c061f58e9c1c58b60c59f1d0c7d589310562514
marcelo paschoal <marcelo.hlp@outlook.com>
2025-09-02 16:19:31 -0300

    fix: making UserCompanyId class serializable

--- src/main/java/com/sevencows/business/model/UserCompanyId.java
+++ src/main/java/com/sevencows/business/model/UserCompanyId.java
@@ -3,6 +3,8 @@ package com.sevencows.business.model;
 import jakarta.persistence.Embeddable;
 import lombok.*;
 
+import java.io.Serializable;
+
 @Embeddable
 @AllArgsConstructor
 @NoArgsConstructor
@@ -10,7 +12,7 @@ import lombok.*;
 @Setter
 @ToString
 @EqualsAndHashCode
-public class UserCompanyId {
+public class UserCompanyId implements Serializable {
 
     private Long user;
     private Long company;
