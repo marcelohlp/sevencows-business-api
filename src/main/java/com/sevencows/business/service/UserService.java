f161cb199ed0c034fb9a8471dde19ccba4d674fc
marcelo paschoal <marcelo.hlp@outlook.com>
2025-09-16 15:34:47 -0300

    feat: add find user by id

--- src/main/java/com/sevencows/business/service/UserService.java
+++ src/main/java/com/sevencows/business/service/UserService.java
@@ -3,12 +3,14 @@ package com.sevencows.business.service;
 import com.sevencows.business.dto.user.UserDtoRegister;
 import com.sevencows.business.model.User;
 import com.sevencows.business.repository.UserRepository;
+import com.sevencows.business.util.OptionalContentExtractor;
 import com.sevencows.business.validation.user.register.ValidateUserRegister;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,13 +18,16 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final List<ValidateUserRegister> validateUserRegisterList;
+    private final OptionalContentExtractor optionalContentExtractor;
 
     public UserService(UserRepository userRepository,
                        PasswordEncoder passwordEncoder,
-                       List<ValidateUserRegister> validateUserRegisterList) {
+                       List<ValidateUserRegister> validateUserRegisterList,
+                       OptionalContentExtractor optionalContentExtractor) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.validateUserRegisterList = validateUserRegisterList;
+        this.optionalContentExtractor = optionalContentExtractor;
     }
 
     protected User addUser(UserDtoRegister userDtoRegister) {
@@ -36,6 +41,12 @@ public class UserService {
 
     protected UserDetails findUserByEmail(String email) {
         return userRepository.findByEmail(email);
+    }
+
+    public User findById(Long id) {
+        Optional<User> optional = userRepository.findById(id);
+        String errorMessage = "User not found by id: " + id;
+        return optionalContentExtractor.getOrThrows(optional, errorMessage);
     }
 
 }
